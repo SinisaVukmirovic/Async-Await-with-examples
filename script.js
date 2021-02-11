d4acd4a6f26b41af7e6bea1db0b2703e8f0fe0b6
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-02-11 23:15:20 +0100

    Simulating async and how fetch works with delay

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Promisses
-// ========================================================================================
+// ==============================================================================
 
 const myPromise = new Promise((resolve, reject) => {
-    // const error = false;
-    const error = true;
+    const error = false;
+    // const error = true;
 
     if (!error) {
         resolve('Yes! Resolved the promise!');
@@ -12,16 +12,34 @@ const myPromise = new Promise((resolve, reject) => {
     }
 });
 
-// console logging Promise obj here returns the state of the promise not ana actual value 
-console.log(myPromise);
+// console logging Promise obj here returns the state of the promise
+// not an actual value 
+// console.log(myPromise);
 
-myPromise
-.then(value => {
-    return value + 123;
-})
-.then(newValue => {
-    console.log(newValue);
-})
-.catch(err => {
-    console.log(err);
+// myPromise
+// .then(value => {
+//     return value + 123;
+// })
+// .then(newValue => {
+//     console.log(newValue);
+// })
+// .catch(err => {
+//     console.log(err);
+// });
+
+// -----------------------------------------------------------
+// simulating how fetch and its delay works with a setTimeout
+const myNextPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('My Next Promise resolved!');
+    }, 3000);
 });
+
+myNextPromise.then(value => {
+    console.log(value);
+});
+
+myPromise.then(value => {
+    console.log(value);
+});
+
