e4702ccbaabfb875f2820400ca6bd176a0da50b5
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-02-12 23:33:08 +0100

    Async Await Example 1

--- js/async-await.js
+++ js/async-await.js
@@ -2,25 +2,50 @@ const myUsers = {
     userList: []
 }
 
-const myCoolFunction = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+// const myCoolFunction = async () => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
-    const userJsonData = await response.json();
+//     const userJsonData = await response.json();
 
-    // console.log(userJsonData);
-    return userJsonData;
-};
+//     // console.log(userJsonData);
+//     return userJsonData;
+// };
 
 // myCoolFunction();
 
-const anotherFunction = async () => {
-    const data = await myCoolFunction();
-    // console.log(data);
+// const anotherFunction = async () => {
+//     const data = await myCoolFunction();
+//     // console.log(data);
 
-    myUsers.userList = data;
+//     myUsers.userList = data;
 
-    console.log(myUsers.userList);
+//     console.log(myUsers.userList);
+// };
+
+// anotherFunction();
+// console.log(myUsers.userList);
+
+// ===============================
+// Example 1
+// ===============================
+const getAllUserEmails = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const jsonUserData = await response.json();
+
+    const userEmailArray = jsonUserData.map(user => {
+        return user.email;
+    });
+
+    // console.log(userEmailArray);
+    // return userEmailArray;
+
+    postToWebPage(userEmailArray);
 };
 
-anotherFunction();
-console.log(myUsers.userList);
+// getAllUserEmails();
+
+const postToWebPage = (data) => {
+    console.log(data);
+};
+
+getAllUserEmails();
