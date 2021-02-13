15453e51ac00aa3556c8341ccfbf1dd93ab00f57
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-02-13 12:52:23 +0100

    Absraction of the example into functions

--- js/request-example.js
+++ js/request-example.js
@@ -1,10 +1,56 @@
-const requestJoke = async (firstName, lastName) => {
-    const response = await fetch(`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}&limitTo=[nerdy]`);
+// const requestJoke = async (firstName, lastName) => {
+//     const response = await fetch(`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}&limitTo=[nerdy]`);
 
-    const jsonResponse = await response.json();
+//     const jsonResponse = await response.json();
 
-    console.log(jsonResponse.value.joke);
+//     console.log(jsonResponse.value.joke);
+// };
+
+// requestJoke('Chuck', 'Norris');
+
+
+// -------------------------------------------------------------
+// Abstraction of the example above into functions
+// -------------------------------------------------------------
+
+// pulling data from a form, for example
+const getDataFromForm = () => {
+    const requestObj = {
+        firstName: 'Bruce',
+        lastName: 'Lee',
+        categories: [
+            'nerdy'
+        ]
+    };
+
+    return requestObj;
 };
 
-requestJoke('Chuck', 'Norris');
+const buildRequestUrl = (requestData) => {
+    return `http://api.icndb.com/jokes/random?firstName=${requestData.firstName}&lastName=${requestData.lastName}&limitTo=${requestData.categories}`;
+};
 
+const requestJoke = async (url) => {
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+
+    const joke = jsonResponse.value.joke;
+    // console.log(joke);
+
+    displayJokeOnPage(joke);
+};
+
+const displayJokeOnPage = (joke) => {
+    console.log(joke);
+};
+
+// Procedural "workflow" function
+const processJokeRequest = async () => {
+    const requestData = getDataFromForm();
+    const requestUrl = buildRequestUrl(requestData);
+    await requestJoke(requestUrl);
+
+    // console.log('Finished!');
+};
+
+processJokeRequest();
